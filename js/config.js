7cdcc540d5651727cf7632a7694b3707f5ff45bb
daniel-thiessen <thiessen.dan@gmail.com>
2025-08-08 05:07:32 +0000

    Deploy site -  (auto-deploy from main)

--- js/config.js
+++ js/config.js
@@ -35,7 +35,7 @@ const siteConfig = {
         items: [
             {
                 id: "choreo1",
-                title: "Choreography Piece 1",
+                title: "Test Title for Choreography Piece 1",
                 thumbnail: "https://images.unsplash.com/photo-1536063211352-0b94219f6212?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
                 fullImage: "https://images.unsplash.com/photo-1536063211352-0b94219f6212?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
                 video: "https://www.youtube.com/embed/dQw4w9WgXcQ",
